16d0189692addd591d750e3c3d352e1ed574b6c2
nana-ikegami <80028527+nana-ikegami@users.noreply.github.com>
2021-03-23 18:11:37 +0900

    新規登録機能

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: { maximum: 40 }
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
+  
+  has_many :comments
+  has_many :orders
+  has_many :items
+end
