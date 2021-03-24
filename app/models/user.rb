b99f94493ddba88d2dc0be274b18f96df7506808
nana-ikegami <80028527+nana-ikegami@users.noreply.github.com>
2021-03-24 14:39:33 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true, length: { maximum: 40 }
 
-  with_options presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ } do
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i } do
     validates :password
     validates :password_confirmation
+  end
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
     validates :family_name
