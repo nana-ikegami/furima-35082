5e8920b0f9d57374189d0226bed5916925c94f85
nana-ikegami <80028527+nana-ikegami@users.noreply.github.com>
2021-03-25 11:54:49 +0900

    出品商品の情報をsquelproに保存できた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :category_id, :user_id, :descuription, :condition_id, :postagetype_id, :preparationday_id, :placeshipment_id, :image).merge(user_id: current_user.id)
   end
 
 end
