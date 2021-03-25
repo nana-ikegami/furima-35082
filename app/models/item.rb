b4ce64e90fc2e574b5830dc1cbfc74d90db2512f
nana-ikegami <80028527+nana-ikegami@users.noreply.github.com>
2021-03-25 15:53:39 +0900

    itemのバリデーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,16 +10,20 @@ class Item < ApplicationRecord
   has_one :order
   has_many :comments
   has_one_attached :image
+  
+  with_options presence: true do
+  validates :name
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+  validates :user
+  validates :descuription
+  validates :image
+  end
 
-  validates :name, presence: true
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
-  validates :category_id, presence: true
-  validates :user, presence: true
-  validates :descuription, presence: true
-  validates :condition_id, presence: true
-  validates :postagetype_id, presence: true
-  validates :preparationday_id, presence: true
-  validates :placeshipment_id, presence: true
-
-  validates :image, presence: true
+  with_options presence: true, numericality: { other_than: 1 }  do
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :postagetype_id, numericality: { other_than: 1 } 
+  validates :preparationday_id, numericality: { other_than: 1 } 
+  validates :placeshipment_id, numericality: { other_than: 1 } 
+  end
 end
