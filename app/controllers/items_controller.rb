0064d2701acc6c5f91baaeffdf6fd05554a2d9f2
nana-ikegami <80028527+nana-ikegami@users.noreply.github.com>
2021-03-26 20:41:58 +0900

    before_action変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
     @items = Item.order("created_at DESC")
   end
