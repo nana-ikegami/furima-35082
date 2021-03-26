5d3adc58c3e5e32a66337d16de9e4230122f3a21
nana-ikegami <80028527+nana-ikegami@users.noreply.github.com>
2021-03-26 18:07:19 +0900

    アクションの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :item_find, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -18,18 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
-    # item.update(item_params)
-
-    if @item.update(item_params)
+   if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
       render :edit
@@ -42,4 +39,14 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :price, :category_id, :user_id, :descuription, :condition_id, :postagetype_id, :preparationday_id, :placeshipment_id, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+
+    def item_find
+      @item = Item.find(params[:id])
+    end
 end
