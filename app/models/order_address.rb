bb0d05f7a014b7bd1fb5bfe7a51e1303289ea9bb
nana-ikegami <80028527+nana-ikegami@users.noreply.github.com>
2021-03-29 12:19:59 +0900

    バリデーション変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,11 +6,11 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ } 
     validates :placeshipment_id
     validates :city
     validates :address_number
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :token
   end
 
