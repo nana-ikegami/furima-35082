f410da8efa10915887c866cf1b58f74868a39f8e
nana-ikegami <80028527+nana-ikegami@users.noreply.github.com>
2021-03-30 12:13:49 +0900

    バリデーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,7 +6,7 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :placeshipment_id
+    validates :placeshipment_id, numericality: { other_than: 1 }
     validates :city
     validates :address_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
